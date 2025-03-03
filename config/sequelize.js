a3776c4bbf8aa4619e2b5dcbe8ec0d05f0a9a154
PetJs <peterfagoroye76@gmail.com>
2025-03-03 22:08:38 +0100

    refactor: :recycle: Refactors database connectivity

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,11 +1,15 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: (...msg) => console.log(msg)
+// Using environment variables
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // Required for Supabase
+      rejectUnauthorized: false, // Bypass self-signed certs
+    },
+  },
+  logging: (...msg) => console.log(msg),
 });
-
 
 module.exports = sequelize;
